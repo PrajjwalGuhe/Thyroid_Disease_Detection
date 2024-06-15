284da227bbbfb444ef01b427f288d8dce1a4a59d
prajjwalguhe <prajwalguhe2000@gmail.com>
2024-06-15 22:38:16 +0530

    mongoDB file updated

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-MONGO_URI = "mongodb+srv://prajjwalguhe:<password>@cluster0.ub6xf4f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_URI = "mongodb+srv://prajjwalguhe:10June2000@cluster0.ub6xf4f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DB_NAME = "thyroidDiseaseDB"
--- mongoDB_setup.py
+++ mongoDB_setup.py
@@ -1,6 +1,6 @@
 import pymongo
 
-MONGO_URI = "mongodb+srv://prajjwalguhe:<Password>@cluster0.ub6xf4f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+MONGO_URI = "mongodb+srv://prajjwalguhe:10June2000@cluster0.ub6xf4f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 DB_NAME = "thyroidDiseaseDB"
 
 client = pymongo.MongoClient(MONGO_URI)
